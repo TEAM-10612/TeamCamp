9a16f0cf6c4858f5bc8835fcea09c3e7271143d9
ryudongjae <rdj10149@gmail.com>
2021-09-20 17:41:52 +0900

    1.거래서비스 도메인 구상(서비스 구상)

---
+++ src/main/java/TeamCamp/demo/domain/model/trade/TradeService.java
@@ -0,0 +1,31 @@
+package TeamCamp.demo.domain.model.trade;
+
+
+import TeamCamp.demo.domain.model.product.Product;
+import TeamCamp.demo.domain.model.users.User;
+import TeamCamp.demo.domain.repository.AddressRepository;
+import TeamCamp.demo.domain.repository.ProductRepository;
+import TeamCamp.demo.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TradeService {
+
+    private final UserRepository userRepository;
+    private final ProductRepository productRepository;
+    private final TradeRepository tradeRepository;
+    private final AddressRepository addressRepository;
+
+    public TradeDto.TradeResource makeTradeResource(User user, Product product){
+        return makeTradeResource(user,product);
+    }
+
+    public void purchase(String email , TradeDto.TradeRequest request){
+
+    }
+    public void sale(String email, TradeDto.TradeRequest request){
+
+    }
+}
