c35c2c93df003e51646e2538f493f92efe8f08d5
ryudongjae <rdj10149@gmail.com>
2021-10-07 20:48:47 +0900

    viewController 분리

--- src/main/java/TeamCamp/Application.java
+++ src/main/java/TeamCamp/Application.java
@@ -3,6 +3,7 @@ package TeamCamp;
 import TeamCamp.demo.common.properties.AppProperties;
 import TeamCamp.demo.common.properties.CacheProperties;
 import TeamCamp.demo.common.s3.AwsProperties;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -14,8 +15,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class Application {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(Application.class)
-                .run(args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
