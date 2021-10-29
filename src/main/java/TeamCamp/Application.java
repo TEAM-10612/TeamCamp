62df69eaa73fd46d2f026394d6873be808b1565b
ryudongjae <rdj10149@gmail.com>
2021-10-29 20:34:40 +0900

    fixed untracked file

--- src/main/java/TeamCamp/Application.java
+++ src/main/java/TeamCamp/Application.java
@@ -15,8 +15,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class Application {
 
     public static final String APPLICATION_LOCATIONS = "spring.config.locations="
-            + "classpath:application.yml"
-            + "/app/config/real-application.yml";
+            + "classpath:application.properties"
+            + "/app/config/real-application.properties";
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(Application.class)
--- src/main/java/TeamCamp/demo/common/config/RedisConfig.java
+++ src/main/java/TeamCamp/demo/common/config/RedisConfig.java
@@ -18,10 +18,10 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 @EnableRedisHttpSession
 public class RedisConfig {
 
-    @Value("${spring:redis:session:host}")
+    @Value("${spring.redis.session.host}")
     private String redisHost;
 
-    @Value("${spring:redis:session:port}")
+    @Value("${spring.redis.session.port}")
     private int redisPort;
 
 
