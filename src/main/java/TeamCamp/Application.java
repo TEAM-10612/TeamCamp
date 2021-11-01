91ffe5bffb881d7c5600e6176c2884f35966cd52
ryudongjae <rdj10149@gmail.com>
2021-11-02 03:25:30 +0900

    배포 자동화 작업

--- src/main/java/TeamCamp/Application.java
+++ src/main/java/TeamCamp/Application.java
@@ -15,7 +15,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class Application {
 
     public static final String APPLICATION_LOCATIONS = "spring.config.locations="
-            + "classpath:application.properties"
+            + "optional:application.properties"
             + "/app/config/application-prod.properties";
 
 
