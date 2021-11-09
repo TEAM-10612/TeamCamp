de25396c3803b65f0465f811bed0e605d3a28f89
ryudongjae <rdj10149@gmail.com>
2021-11-09 16:43:55 +0900

    Mysql master/slave 분리

--- src/main/java/TeamCamp/demo/test/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package TeamCamp.demo.test;
-
-import org.apache.poi.ss.formula.ptg.MemErrPtg;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MemberRepository extends JpaRepository<Member,Long> {
-}
